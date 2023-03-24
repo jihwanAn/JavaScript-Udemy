46ccfc65e640e0f1c15fee7658d5c1981f46dde2
jihwanAn <an0572@naver.com>
2023-03-24 17:49:49 +0900

    DOM 요소 이동시키기

--- Practice-oop/assets/scripts/app.js
+++ Practice-oop/assets/scripts/app.js
@@ -1,18 +1,43 @@
+// 요소를 교체하는 함수
+class DOMHelper {
+  static clearEventListeners(element) {
+    const clonedElement = element.cloneNode(true); // 깊은 복제
+    element.replaceWith(clonedElement);
+    return clonedElement;
+  }
+
+  static moveElement(elenmentId, newDestinationSelector) {
+    const element = document.getElementById(elenmentId);
+    const destinationElement = document.querySelector(newDestinationSelector);
+    destinationElement.append(element);
+  }
+}
+
 class Tooltip {}
 
 class ProjectItem {
-  constructor(id, updateProjectListsFunction) {
+  constructor(id, updateProjectListsFunction, type) {
     this.id = id;
     this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
-    this.connectSwitchButton();
+    this.connectSwitchButton(type);
   }
   connectMoreInfoButton() {}
 
-  connectSwitchButton() {
+  connectSwitchButton(type) {
     const projectItemElement = document.getElementById(this.id);
-    const switchBtn = projectItemElement.querySelector("button:last-of-type");
-    switchBtn.addEventListener("click", this.updateProjectListsHandler);
+    let switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn = DOMHelper.clearEventListeners(switchBtn);
+    switchBtn.textContent = type === "active" ? "Finished" : "Active";
+    // 기존 이벤트 리스너는 삭제, 새로운 리스너만 추가 => 시간이 지나도 이벤트 리스너가 누적되지 않게 됨
+    switchBtn.addEventListener(
+      "click",
+      this.updateProjectListsHandler.bind(null, this.id)
+    );
+  }
+  update(updateProjectListsFn, type) {
+    this.updateProjectListsHandler = updateProjectListsFn;
+    this.connectSwitchButton(type);
   }
 }
 
@@ -21,10 +46,11 @@ class ProjectList {
 
   constructor(type) {
     this.type = type;
+
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
       this.projects.push(
-        new ProjectItem(prjItem.id, this.swichProject.bind(this))
+        new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
       );
     }
     console.log(this.projects);
@@ -34,11 +60,13 @@ class ProjectList {
     this.switchHandler = switchHandlerFunction;
   }
 
-  addProject() {
-    console.log(this);
+  addProject(project) {
+    this.projects.push(project);
+    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    project.update(this.switchProject.bind(this), this.type);
   }
 
-  swichProject(projectId) {
+  switchProject(projectId) {
     // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
     // this.projects.splice(projectIndex, 1);
     this.switchHandler(this.projects.find((p) => p.id === projectId));
